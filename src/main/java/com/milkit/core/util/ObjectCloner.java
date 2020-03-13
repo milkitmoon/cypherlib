289493c343eafd1d51f01e585cebab44cb5ad439
unknown <milkit.moon@gmail.com>
2020-03-13 15:35:25 +0900

    code cleanance

--- src/main/java/com/milkit/core/util/ObjectCloner.java
+++ src/main/java/com/milkit/core/util/ObjectCloner.java
@@ -6,37 +6,35 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class ObjectCloner {
-	   // so that nobody can accidentally create an ObjectCloner object
-	   private ObjectCloner(){}
-	   // returns a deep copy of an object
-	   static public Object deepCopy(Object oldObj) throws Exception
-	   {
-	      ObjectOutputStream oos = null;
-	      ObjectInputStream ois = null;
-	      try
-	      {
-	         ByteArrayOutputStream bos = new ByteArrayOutputStream(); // A
-	         oos = new ObjectOutputStream(bos); // B
-	         // serialize and pass the object
-	         oos.writeObject(oldObj);   // C
-	         oos.flush();               // D
-	         ByteArrayInputStream bin = new ByteArrayInputStream(bos.toByteArray()); // E
-	         ois = new ObjectInputStream(bin);                  // F
-	         // return the new object
-	         return ois.readObject(); // G
-	      }
-	      catch(Exception e)
-	      {
-	         throw(e);
-	      }
-	      finally
-	      {
-	    	 if(oos != null) {
-	    		 oos.close();
-	    	 }
-	    	 if(ois != null) {
-	    		 ois.close();
-	    	 }
-	      }
-	   }
+
+	private ObjectCloner() {}
+	
+	// returns a deep copy of object
+	static public Object deepCopy(Object oldObj) throws Exception {
+		ObjectOutputStream oos = null;
+		ObjectInputStream ois = null;
+	    
+		try {
+	    	ByteArrayOutputStream bos = new ByteArrayOutputStream();
+	    	oos = new ObjectOutputStream(bos);
+	    	
+	    	// serialize and pass the object
+	    	oos.writeObject(oldObj);
+	    	oos.flush();
+	    	ByteArrayInputStream bin = new ByteArrayInputStream(bos.toByteArray());
+	    	ois = new ObjectInputStream(bin);
+	    	
+	    	// return the new object
+	    	return ois.readObject();
+	    } catch(Exception e) {
+	    	throw(e);
+	    } finally {
+	    	if(oos != null) {
+	    		oos.close();
+	    	}
+	    	if(ois != null) {
+	    		ois.close();
+	    	}
+	    }
+	}
 }
